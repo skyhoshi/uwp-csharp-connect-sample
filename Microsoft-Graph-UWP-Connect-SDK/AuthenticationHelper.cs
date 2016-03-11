36d3fe50f153d321ad8488f0f292eb2c4278b599
Jim Crowley <jamescro@microsoft.com>
2016-03-10 16:51:46 -0800

    changing directory names

---
+++ Microsoft-Graph-UWP-Connect-SDK/AuthenticationHelper.cs
@@ -0,0 +1,67 @@
+﻿//Copyright (c) Microsoft. All rights reserved. Licensed under the MIT license.
+//See LICENSE in the project root for license information.
+
+using System;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Linq;
+using System.Threading.Tasks;
+using Windows.Security.Authentication.Web;
+using Windows.Security.Authentication.Web.Core;
+using Windows.Security.Credentials;
+using Windows.Storage;
+using Microsoft.Graph.Authentication;
+using Microsoft.Graph;
+
+namespace Microsoft_Graph_UWP_Connect_SDK
+{
+    internal static class AuthenticationHelper
+    {
+        // The Client ID is used by the application to uniquely identify itself to Microsoft Azure Active Directory (AD).
+        static string clientId = App.Current.Resources["ida:ClientID"].ToString();
+
+        static string returnUrl = App.Current.Resources["ida:ReturnUrl"].ToString();
+
+        private static GraphServiceClient graphClient = null;
+
+        // Get an access token for the given context and resourceId. An attempt is first made to 
+        // acquire the token silently. If that fails, then we try to acquire the token by prompting the user.
+        public static async Task<GraphServiceClient> GetAuthenticatedClientAsync()
+        {
+            if (graphClient == null)
+            {
+                var authenticationProvider = new OAuth2AuthenticationProvider(
+                    new AppConfig
+                    {
+                        ClientId = clientId,
+                        ReturnUrl = returnUrl,
+                        Scopes = new string[]
+                        {
+                        "openid",
+                        "offline_access",
+                        "https://graph.microsoft.com/User.Read",
+                        "https://graph.microsoft.com/Mail.Send",
+                        },
+                    });
+
+                await authenticationProvider.AuthenticateAsync();
+
+                graphClient = new GraphServiceClient(authenticationProvider);
+            }
+
+            return graphClient;
+        }
+
+        /// <summary>
+        /// Signs the user out of the service.
+        /// </summary>
+        public static void SignOut()
+        {
+            //Clear stored values from last authentication.
+            graphClient.Dispose();
+            graphClient = null;
+
+        }
+
+    }
+}
