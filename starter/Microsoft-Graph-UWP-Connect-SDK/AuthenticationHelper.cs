a1820b67baf034e8335a1a43aace02b09715acfc
Jim Crowley <jamescro@microsoft.com>
2016-09-14 14:33:32 -0700

    adding starter project

---
+++ starter/Microsoft-Graph-UWP-Connect-SDK/AuthenticationHelper.cs
@@ -0,0 +1,56 @@
+﻿//Copyright (c) Microsoft. All rights reserved. Licensed under the MIT license.
+//See LICENSE in the project root for license information.
+
+using System;
+using System.Diagnostics;
+using System.Net.Http.Headers;
+using System.Net.Http;
+using System.Linq;
+using System.Threading.Tasks;
+using Windows.Security.Authentication.Web;
+using Windows.Security.Authentication.Web.Core;
+using Windows.Security.Credentials;
+using Windows.Storage;
+using Microsoft.Graph;
+using Microsoft.Identity.Client;
+
+namespace Microsoft_Graph_UWP_Connect_SDK
+{
+    public class AuthenticationHelper
+    {
+        // The Client ID is used by the application to uniquely identify itself to the v2.0 authentication endpoint.
+        static string clientId = App.Current.Resources["ida:ClientID"].ToString();
+        public static string[] Scopes = { "https://graph.microsoft.com/User.Read", "https://graph.microsoft.com/Mail.Send" };
+        public static PublicClientApplication IdentityClientApp = new PublicClientApplication(clientId);
+        public static string TokenForUser = null;
+        public static DateTimeOffset Expiration;
+        private static GraphServiceClient graphClient = null;
+
+
+        // Get an access token for the given context and resourceId. An attempt is first made to 
+        // acquire the token silently. If that fails, then we try to acquire the token by prompting the user.
+        public static GraphServiceClient GetAuthenticatedClient()
+        {
+
+        }
+
+
+        /// <summary>
+        /// Get Token for User.
+        /// </summary>
+        /// <returns>Token for user.</returns>
+        public static async Task<string> GetTokenForUserAsync()
+        {
+
+        }
+
+        /// <summary>
+        /// Signs the user out of the service.
+        /// </summary>
+        public static void SignOut()
+        {
+
+        }
+
+    }
+}
---
+++ starter/Microsoft-Graph-UWP-Connect-SDK/MailHelper.cs
@@ -0,0 +1,32 @@
+﻿//Copyright (c) Microsoft. All rights reserved. Licensed under the MIT license.
+//See LICENSE in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Microsoft.Graph;
+using System.Net.Http.Headers;
+
+namespace Microsoft_Graph_UWP_Connect_SDK
+{
+    public class MailHelper
+    {
+        /// <summary>
+        /// Compose and send a new email.
+        /// </summary>
+        /// <param name="subject">The subject line of the email.</param>
+        /// <param name="bodyContent">The body of the email.</param>
+        /// <param name="recipients">A semicolon-separated list of email addresses.</param>
+        /// <returns></returns>
+        public async Task ComposeAndSendMailAsync(string subject,
+                                                            string bodyContent,
+                                                            string recipients)
+        {
+
+
+        }
+    }
+}
