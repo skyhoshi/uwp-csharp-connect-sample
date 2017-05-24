8b246b66d103a77e9011d0d499074f302e830cfd
Jim Crowley <jamescro@microsoft.com>
2017-05-24 13:42:39 -0700

    Updating starter project for new MSAL

--- starter/Microsoft-Graph-UWP-Connect-SDK/AuthenticationHelper.cs
+++ starter/Microsoft-Graph-UWP-Connect-SDK/AuthenticationHelper.cs
@@ -21,17 +21,44 @@ namespace Microsoft_Graph_UWP_Connect_SDK
         // The Client ID is used by the application to uniquely identify itself to the v2.0 authentication endpoint.
         static string clientId = App.Current.Resources["ida:ClientID"].ToString();
         public static string[] Scopes = { "User.Read", "Mail.Send", "Files.ReadWrite" };
+
         public static PublicClientApplication IdentityClientApp = new PublicClientApplication(clientId);
+
         public static string TokenForUser = null;
         public static DateTimeOffset Expiration;
-        private static GraphServiceClient graphClient = null;
 
+        private static GraphServiceClient graphClient = null;
 
         // Get an access token for the given context and resourceId. An attempt is first made to 
         // acquire the token silently. If that fails, then we try to acquire the token by prompting the user.
         public static GraphServiceClient GetAuthenticatedClient()
         {
+            if (graphClient == null)
+            {
+                // Create Microsoft Graph client.
+                try
+                {
+                    graphClient = new GraphServiceClient(
+                        "https://graph.microsoft.com/v1.0",
+                        new DelegateAuthenticationProvider(
+                            async (requestMessage) =>
+                            {
+                                var token = await GetTokenForUserAsync();
+                                requestMessage.Headers.Authorization = new AuthenticationHeaderValue("bearer", token);
+                                // This header has been added to identify our sample in the Microsoft Graph service.  If extracting this code for your project please remove.
+                                requestMessage.Headers.Add("SampleID", "uwp-csharp-connect-sample");
 
+                            }));
+                    return graphClient;
+                }
+
+                catch (Exception ex)
+                {
+                    Debug.WriteLine("Could not create a graph client: " + ex.Message);
+                }
+            }
+
+            return graphClient;
         }
 
 
@@ -41,7 +68,25 @@ namespace Microsoft_Graph_UWP_Connect_SDK
         /// <returns>Token for user.</returns>
         public static async Task<string> GetTokenForUserAsync()
         {
+            AuthenticationResult authResult;
+            try
+            {
+                authResult = await IdentityClientApp.AcquireTokenSilentAsync(Scopes, IdentityClientApp.Users.First());
+                TokenForUser = authResult.AccessToken;
+            }
 
+            catch (Exception)
+            {
+                if (TokenForUser == null || Expiration <= DateTimeOffset.UtcNow.AddMinutes(5))
+                {
+                    authResult = await IdentityClientApp.AcquireTokenAsync(Scopes);
+
+                    TokenForUser = authResult.AccessToken;
+                    Expiration = authResult.ExpiresOn;
+                }
+            }
+
+            return TokenForUser;
         }
 
         /// <summary>
@@ -49,6 +94,12 @@ namespace Microsoft_Graph_UWP_Connect_SDK
         /// </summary>
         public static void SignOut()
         {
+            foreach (var user in IdentityClientApp.Users)
+            {
+                IdentityClientApp.Remove(user);
+            }
+            graphClient = null;
+            TokenForUser = null;
 
         }
 
